56f98788241f709948deb20acdb278ae55baae4a
Sneha0024 <77921781+Sneha0024@users.noreply.github.com>
2021-05-08 13:27:36 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreditCardPaymentStoreEffects } from './store/effects';
-import { CardPaymentComponent } from './card-payment/card-payment.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 import { PaymentService } from './services/payment.service';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
@@ -23,7 +23,7 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
-    CardPaymentComponent,
+    UserDetailsComponent,
   ],
   imports: [
     BrowserModule,
